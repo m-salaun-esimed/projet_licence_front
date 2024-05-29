1ef12631708f20e6b02e7b32c55d837e90c6716c
m-salaun-esimed <msalaun@esimed.fr>
2024-05-29 08:19:41 +0200

    correction bug

--- app/controllers/creationCompte.js
+++ app/controllers/creationCompte.js
@@ -7,40 +7,6 @@ class CreationCompteController extends BaseController {
         this.userModel = new UserModel()
         document.getElementById("erreur").textContent = ""
         document.getElementById("erreur").style.display = "none"
-        const passwordInput = document.querySelector(".pass-field input");
-        const eyeIcon = document.querySelector(".pass-field i");
-        const requirementList = document.querySelectorAll(".requirement-list li");
-
-// An array of password requirements with corresponding
-// regular expressions and index of the requirement list item
-        const requirements = [
-            { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
-            { regex: /[0-9]/, index: 1 }, // At least one number
-            { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
-            { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
-            { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
-        ]
-
-        passwordInput.addEventListener("keyup", (e) => {
-            requirements.forEach(item => {
-                const isValid = item.regex.test(e.target.value);
-                const requirementItem = requirementList[item.index];
-
-                if (isValid) {
-                    requirementItem.classList.add("valid");
-                    requirementItem.firstElementChild.className = "fa-solid fa-check";
-                } else {
-                    requirementItem.classList.remove("valid");
-                    requirementItem.firstElementChild.className = "fa-solid fa-circle";
-                }
-            });
-        });
-
-        eyeIcon.addEventListener("click", () => {
-            // Toggle the password input type between "password" and "text"
-            passwordInput.type = passwordInput.type === "password" ? "text" : "password";
-            eyeIcon.className = `fa-solid fa-eye${passwordInput.type === "password" ? "" : "-slash"}`;
-        });
     }
 
     pageConnexion(){
