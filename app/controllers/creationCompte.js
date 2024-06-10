3828165f89278730e526561dbe68bf7f80bbd334
m-salaun-esimed <msalaun@esimed.fr>
2024-06-10 08:40:53 +0200

    redirection page de connexion apres création de compte

--- app/controllers/creationCompte.js
+++ app/controllers/creationCompte.js
@@ -19,17 +19,15 @@ class CreationCompteController extends BaseController {
         const password = document.getElementById("password").value.trim();
         const password2 = document.getElementById("password2").value.trim();
 
-        // Validate required fields
         if (!displayName || !login || !password || !password2) {
             this.displayError("Veuillez remplir tout les champs");
             return;
         }
         if (displayName.length > 12){
-            alert("Nom d'utilisateur trop long maximun 12 caractères")
+            alert("Nom d'utilisateur trop long maximun 12 caractères");
             return;
         }
 
-        // Validate email format
         const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!regexEmail.test(login)) {
             this.displayError("email non comforme : format adresse mail");
@@ -47,6 +45,7 @@ class CreationCompteController extends BaseController {
             if (response.ok) {
                 document.getElementById("erreur").style.display = "none";
                 alert("Création du compte réussi !")
+                navigate("index")
             } else {
                 console.log("Échec Creation Compte");
                 const errorData = await response.json();
