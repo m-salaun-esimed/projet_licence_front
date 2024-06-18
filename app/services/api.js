bb03666075ea33caf2819e93bf5e46a29b3861f6
m-salaun-esimed <msalaun@esimed.fr>
2024-06-18 15:55:41 +0200

    nouveau back

--- app/services/api.js
+++ app/services/api.js
@@ -1,6 +1,6 @@
 export default class Api {
     constructor() {
-        this.apiServer = "https://www.main-bvxea6i-ilzfunk6nmoog.fr-4.platformsh.site/";
+        this.apiServer = "https://www.main-bvxea6i-ilzfunk6nmoog.fr-4.platformsh.site";
         // this.apiServer = "http://localhost:3333";
 
         this.myHeaders = new Headers({
